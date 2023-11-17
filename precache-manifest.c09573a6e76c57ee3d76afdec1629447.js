86db197698c445667a60381951b48dbed8bf4747
roman.mironovich <roman.mironovich@am-intech.ru>
2023-11-17 19:48:56 +0300

    Updates

---
+++ precache-manifest.c09573a6e76c57ee3d76afdec1629447.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce54e4860ac80ca463c0",
+    "url": "/text-delimiter/static/css/main.880b76e4.chunk.css"
+  },
+  {
+    "revision": "ce54e4860ac80ca463c0",
+    "url": "/text-delimiter/static/js/main.2e69fff0.chunk.js"
+  },
+  {
+    "revision": "9e76c55dd28c66c5d159",
+    "url": "/text-delimiter/static/js/runtime~main.51a06460.js"
+  },
+  {
+    "revision": "676a0a9f7719b31a1271",
+    "url": "/text-delimiter/static/css/2.aa85d5ca.chunk.css"
+  },
+  {
+    "revision": "676a0a9f7719b31a1271",
+    "url": "/text-delimiter/static/js/2.632af4c0.chunk.js"
+  },
+  {
+    "revision": "0844b197c4f18213fc3a97e703f1acd8",
+    "url": "/text-delimiter/static/media/settings.0844b197.svg"
+  },
+  {
+    "revision": "26507f8e38e05d86d4980f09f01ea66e",
+    "url": "/text-delimiter/static/media/ProximaNova-Bold.26507f8e.woff2"
+  },
+  {
+    "revision": "335cb3e72fa421c3a282518f17bc651a",
+    "url": "/text-delimiter/static/media/ProximaNova-Extrabld.335cb3e7.woff2"
+  },
+  {
+    "revision": "99e5ce70ce097be8d30908bad7f82d00",
+    "url": "/text-delimiter/static/media/ProximaNova-Extrabld.99e5ce70.woff"
+  },
+  {
+    "revision": "051acd0f3d207795179cd71fcf44eb6c",
+    "url": "/text-delimiter/static/media/ProximaNova-Light.051acd0f.woff2"
+  },
+  {
+    "revision": "651c239adf29e56fd46137c9fef0b191",
+    "url": "/text-delimiter/static/media/ProximaNova-Regular.651c239a.woff"
+  },
+  {
+    "revision": "f5a7065fa932ee3cd45bd5b7ae0d1ea3",
+    "url": "/text-delimiter/static/media/ProximaNova-Bold.f5a7065f.woff"
+  },
+  {
+    "revision": "0071b00b4d686a9bf227e289c40d6f55",
+    "url": "/text-delimiter/static/media/ProximaNova-Regular.0071b00b.woff2"
+  },
+  {
+    "revision": "b1d7fb702e1dfa48213261ff62730fc8",
+    "url": "/text-delimiter/static/media/ProximaNova-Black.b1d7fb70.woff2"
+  },
+  {
+    "revision": "2b7e90a1a0cd4e8537da505db584a011",
+    "url": "/text-delimiter/static/media/ProximaNova-Light.2b7e90a1.woff"
+  },
+  {
+    "revision": "a327ebd3f9aaa6d3f6e96442e9b3fa5a",
+    "url": "/text-delimiter/static/media/ProximaNova-Black.a327ebd3.woff"
+  },
+  {
+    "revision": "3a87a7096ca869385dcf5a5ac1997284",
+    "url": "/text-delimiter/index.html"
+  }
+];
